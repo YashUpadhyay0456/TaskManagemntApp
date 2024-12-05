31328f7117aebb65510e0f367d0d520faa694a55
Yash Upadhyay <yashuchanu@gmail.com>
2024-12-05 12:28:57 +0530

    initial setup of app

---
+++ TaskManagemntApp/src/main/java/com/TaskManagemntApp/TaskManagemntApp/TaskManagemntAppApplication.java
@@ -0,0 +1,13 @@
+package com.TaskManagemntApp.TaskManagemntApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagemntAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagemntAppApplication.class, args);
+	}
+
+}
---
+++ TaskManagemntApp/src/test/java/com/TaskManagemntApp/TaskManagemntApp/TaskManagemntAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskManagemntApp.TaskManagemntApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagemntAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
